2e89f66a8e2773588cc53a2d5982a9464317d535
oscarrobertson <or13827@my.bristol.ac.uk>
2015-06-01 15:12:34 +0100

    Finished file renaming and added data files
    That's a lot of ascii

--- fileMaker.py
+++ fileMaker.py
@@ -1,7 +1,18 @@
 import os
 
-rootdir = 'C:\Users\Oscar\Downloads\Terrain Data\data\data'
+##renames the orignal ascii files into a more useful format
 
-for files in os.walk(rootdir):
-    for file in files:
-        print file
+rootdir = 'C:\Users\Oscar\Downloads\Terrain Data\data\data\\'
+
+for filename in os.listdir(rootdir):
+    xllcorner = ""
+    yllcorner = ""
+    with open(rootdir + filename,'r') as f:
+        ncols = int(f.readline()[6:])
+        nrows = int(f.readline()[6:])
+        xllcorner = f.readline()[10:].rstrip()
+        yllcorner = f.readline()[10:].rstrip()
+        cellsize = int(f.readline()[9:])
+    os.rename(rootdir + filename, rootdir + xllcorner + '-' + yllcorner + '.asc')
+    if ncols != 200 or nrows != 200 or cellsize != 50:
+        print filename
