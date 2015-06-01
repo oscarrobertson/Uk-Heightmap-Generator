aa37878ffc9505013d8eb7e7d8494298ad760e04
oscarrobertson <or13827@my.bristol.ac.uk>
2015-06-01 18:48:54 +0100

    Builds array for regions which contain the data
    Now just needs to be refined to be the exact specified area and contrast
    set so png can be printed

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import sys
 ##GLOBALS
 ELEMENT_WIDTH = 50
 REGION_WIDTH_IN_ELEMENTS = 200
-REGION_WIDTH = ELEMENT_WIDTH * REGION_WIDTH_IN_ELEMENT
+REGION_WIDTH = ELEMENT_WIDTH * REGION_WIDTH_IN_ELEMENTS
 
 ## Identifies all regions in a given square
-## and returns the filenames as a list
+## Returns list of coordinates of lower left corner of all regions
 def findRegions(x,y,w):
     xll = x/REGION_WIDTH * REGION_WIDTH
     yll = y/REGION_WIDTH * REGION_WIDTH
@@ -26,20 +26,95 @@ def findRegions(x,y,w):
     output = []
     for xtemp in xregions:
         for ytemp in yregions:
-            output.append('data\\' + str(xtemp) + '-' + str(ytemp) + '.asc')
+            output.append((str(xtemp),str(ytemp)))
     return output
 
-def main():
-    ##get the regioin that needs to be created
-    xll = 108000
-    yll = 102000
-    width = 12000
+def makeFilename(x,y):
+    return 'data\\' + str(x) + '-' + str(y) + '.asc'
 
-    ## build list of filenames where the data lies
-    regionsNeeded = findRegions(xll,yll,width)
+## build 2d array for a region
+def makeArrayForRegion(regionCoord):
+    output = []
+    filename = makeFilename(regionCoord[0],regionCoord[1])
 
-    ## build the 2d array of all squraes where the requested data exists
+    with open(filename,'r') as f:
+        ncols = int(f.readline()[6:])
+        nrows = int(f.readline()[6:])
+        xllcorner = int(f.readline()[10:])
+        yllcorner = int(f.readline()[10:])
+        cellsize = int(f.readline()[9:])
+        
+        for i in range(nrows):
+            newRow = [int(float(k)) for k in f.readline().split()]
+            output.append(newRow)
+            newRow = []
+
+    return output
+
+def appendMatrixLR(left,right):
+    if len(left) != len(right):
+        print "Matrix Dimension error"
+        return False
+    output = []
+    for i in range(len(left)):
+        newRow = left[i] + right[i]
+        output.append(newRow)
+    return output
+
+def appendMatrixTB(top,bottom):
+    ## basic check, not fully safe
+    if len(top[0]) != len(bottom[0]):
+        print "Matrix dimension error"
+        return False
+    output = top + bottom
+    return output
+
+def combineToColumns(regions,coords):
+    output = []
+    currentX = '-1'
+    for i in range(len(regions)):
+        ## if equal to last one then append
+        if coords[i][0] == str(currentX):
+            output[len(output)-1] = appendMatrixTB(regions[i],output[len(output)-1])
+        else:
+            output.append(regions[i])
+            currentX = coords[i][0]
+    return output
+
+def combineToSquare(regions):
+    output = regions[0]
+    for i in range(1,len(regions)):
+        output = appendMatrixLR(output,regions[i])
+    return output
+
+## input list of coordinates
+## outputs 2d array of the combination of regions
+def createDataArray(regionsNeeded):
+    ## first construct a 2d array for each region and place in new list
+    ## this list can be referenced using the regionsNeeded list
+    regions = []
+    for i in regionsNeeded:
+        regions.append(makeArrayForRegion(i))
+
+    ## combine regions into columns
+    regions = combineToColumns(regions,regionsNeeded)
+
+    ## combine columns into square
+    regions = combineToSquare(regions)
     
+    return regions
+
+def main():
+    ##get the region that needs to be created
+    xll = 400000
+    yll = 200000
+    width = 30000
+
+    ## build list of coordinates where the data lies
+    regionsNeeded = findRegions(xll,yll,width)
+  
+    ## build the 2d array of all squraes where the requested data exists
+    dataArray = createDataArray(regionsNeeded)
     
 
 main()
